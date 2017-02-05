300b2bcdcc4e9bcc39ba55d7bb85bfec160093c2
Drew Grubb <drew.grubb@runtimedevelopment.com>
2017-02-04 20:43:32 -0800

    added a slideshow to the home page. split javascript into 2 separate files. renamed a handful of css classes

---
+++ scripts/ownership.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	setYearRange();
+});
+
+function setYearRange()
+{
+	var currentYear = (new Date).getFullYear();
+	var yearText = "2017";
+	if (currentYear > 2017)
+	{
+		yearText = yearText + " - " + currentYear;
+	}
+	yearText += " - Drew Grubb";
+
+	$('#ownership').text(yearText);
+}
